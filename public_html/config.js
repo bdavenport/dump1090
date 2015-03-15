7cdd3aa810f7f6d43cc275481151426012886f49
steel101 <steelvargas@gmail.com>
2015-03-15 14:37:40 -0500

    Update config.js

--- public_html/config.js
+++ public_html/config.js
@@ -11,8 +11,8 @@ Metric = false; // true or false
 
 // -- Map settings ----------------------------------------
 // The Latitude and Longitude in decimal format
-CONST_CENTERLAT = 30.384;
-CONST_CENTERLON = 262.293;
+CONST_CENTERLAT = 1.384;
+CONST_CENTERLON = 1.293;
 // The google maps zoom level, 0 - 16, lower is further out
 CONST_ZOOMLVL   = 9;
 StandardMapType = 'OSM2'; // mapType[google.maps.MapTypeId.ROADMAP|google.maps.MapTypeId.TERRAIN|google.maps.MapTypeId.SATELLITE|'OSM'|'OSM2'|'OAIP'|'dark_map']. google mapTypeIDs without ' ' eg. google.maps.MapTypeId.ROADMAP
@@ -26,8 +26,8 @@ StaleColor = "rgb(255, 0, 0)";
 // -- Site Settings ---------------------------------------
 SiteShow    = true; // true or false
 // The Latitude and Longitude in decimal format
-SiteLat     = 30.384;
-SiteLon     = 262.293;
+SiteLat     = 1.384;
+SiteLon     = 1.293;
 
 SiteCircles = true; // true or false (Only shown if SiteShow is true)
 // In nautical miles or km (depending settings value 'Metric')
