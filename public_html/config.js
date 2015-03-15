977e9b5d482aff156c0ec80051728cec6156a5a8
steel101 <steelvargas@gmail.com>
2015-03-15 14:27:03 -0500

    Update config.js

--- public_html/config.js
+++ public_html/config.js
@@ -11,24 +11,28 @@ Metric = false; // true or false
 
 // -- Map settings ----------------------------------------
 // The Latitude and Longitude in decimal format
-CONST_CENTERLAT = 45.0;
-CONST_CENTERLON = 9.0;
+CONST_CENTERLAT = 30.384;
+CONST_CENTERLON = 262.293;
 // The google maps zoom level, 0 - 16, lower is further out
-CONST_ZOOMLVL   = 5;
+CONST_ZOOMLVL   = 9;
+StandardMapType = 'OSM2'; // mapType[google.maps.MapTypeId.ROADMAP|google.maps.MapTypeId.TERRAIN|google.maps.MapTypeId.SATELLITE|'OSM'|'OSM2'|'OAIP'|'dark_map']. google mapTypeIDs without ' ' eg. google.maps.MapTypeId.ROADMAP
 
 // -- Marker settings -------------------------------------
 // The default marker color
-MarkerColor	  = "rgb(127, 127, 127)";
+MarkerColor	  = "rgb(0, 255, 0)";
 SelectedColor = "rgb(225, 225, 225)";
-StaleColor = "rgb(190, 190, 190)";
+StaleColor = "rgb(255, 0, 0)";
 
 // -- Site Settings ---------------------------------------
-SiteShow    = false; // true or false
+SiteShow    = true; // true or false
 // The Latitude and Longitude in decimal format
-SiteLat     = 45.0;
-SiteLon     = 9.0;
+SiteLat     = 30.384;
+SiteLon     = 262.293;
 
 SiteCircles = true; // true or false (Only shown if SiteShow is true)
 // In nautical miles or km (depending settings value 'Metric')
-SiteCirclesDistances = new Array(100,150,200);
+SiteCirclesDistances = new Array(5,10,15,20,25,30,35,40,45,50,55,60);
 
+// Extended Airplane Marker
+airplanemarker_showextended = true;
+airplanemarker_showtext     = true;
