b7d7e244b6cef27340c06c3218683c0210f4820b
steel johnson <steelvargas@gmail.com>
2015-03-22 19:08:37 -0500

    Update config.js

--- public_html/js/settings/config.js
+++ public_html/js/settings/config.js
@@ -5,37 +5,37 @@
 // Load this file before script.js file at gmap.html.
 //defalt settings can change in settings page
 function set_reset(test) {
-    storevalue('flightawareshow','true');
-    storevalue('fr24show','true');
-    storevalue('flightstatsshow','true');
-    storevalue('maplat','30.384');
-    storevalue('maplon','262.300');
-    storevalue('sitelat','30.384');
-    storevalue('sitelon','262.294');
-    storevalue('1r','0');//active planes red of rgb
-    storevalue('1g','255');//active planes green in rgb
-    storevalue('1b','0');//active planes blue in rgb
-    storevalue('2r','30');//slected planes red in rgb      
-    storevalue('2g','30');//slected planes green in rgb
-    storevalue('2b','30');//slected planes blue in rgb
-    storevalue('3r','255');//non active planes red in rgb
-    storevalue('3g','0');//non active planes green in rgb
-    storevalue('3b','0');//non active planes blue in rgb
-    storevalue('setr','0');//truecolor red of rgb
-    storevalue('setg','255');//truecolor green in rgb
-    storevalue('setb','0');//truecolor blue in rgb
-    storevalue('set2r','255');//falsecolor red in rgb      
-    storevalue('set2g','0');//falsecolor green in rgb
-    storevalue('set2b','0');//falsecolor blue in rgb
-    storevalue('metric','false');
-    storevalue('showsite','true');
-    storevalue('showcircles','true');
-    storevalue('airplanemarker_showextended','true');
-    storevalue('airplanemarker_showtext','true');
-    storevalue('zoom',9);
-    storevalue('darkmode','true');
-    storevalue('coolclock','true');
-    storevalue('normalclock','true');
+    storevalue('flightawareshow','true');//can change on settings page link to flght info 
+    storevalue('fr24show','true');//can change on settings page  page link to flght info 
+    storevalue('flightstatsshow','true');//can change on settings page  page link to flght info 
+    storevalue('maplat','30.384');//defalt value for map lat can change on settings page 
+    storevalue('maplon','262.300');//defalt value for map lon can change on settings page 
+    storevalue('sitelat','30.384');//defalt value for site lat can change on settings page 
+    storevalue('sitelon','262.294');//defalt value for site lon can change on settings page 
+    storevalue('1r','0');//active planes red of rgb can change on settings page 
+    storevalue('1g','255');//active planes green in rgb can change on settings page 
+    storevalue('1b','0');//active planes blue in rgb can change on settings page 
+    storevalue('2r','30');//slected planes red in rgb   can change on settings page     
+    storevalue('2g','30');//slected planes green in rgb can change on settings page 
+    storevalue('2b','30');//slected planes blue in rgb can change on settings page  
+    storevalue('3r','255');//non active planes red in rgb can change on settings page 
+    storevalue('3g','0');//non active planes green in rgb can change on settings page 
+    storevalue('3b','0');//non active planes blue in rgb can change on settings page 
+    storevalue('setr','0');//truecolor red of rgb can change on settings page 
+    storevalue('setg','255');//truecolor green in rgb can change on settings page 
+    storevalue('setb','0');//truecolor blue in rgb can change on settings page 
+    storevalue('set2r','255');//falsecolor red in rgb can change on settings page 
+    storevalue('set2g','0');//falsecolor green in rgb can change on settings page 
+    storevalue('set2b','0');//falsecolor blue in rgb can change on settings page 
+    storevalue('metric','');//can change on settings page
+    storevalue('showsite','true');//can change on settings page
+    storevalue('showcircles','true');//can change on settings page
+    storevalue('airplanemarker_showextended','true');//can change on settings page
+    storevalue('airplanemarker_showtext','true');//can change on settings page
+    storevalue('zoom',9);//can change on settings page
+    storevalue('darkmode','true');//can change on settings pag
+    storevalue('coolclock','true');//can change on settings page
+    storevalue('normalclock','');//can change on settings pag//can change on settings page
     return true;
     
 }
